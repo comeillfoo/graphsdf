5dce6925d06b3d40d7b1c25cef7b079d5d112f07
comeillfoo <rayveleaveme34@inbox.ru>
2022-12-22 22:09:03 +0000

    feat: IDENTIFIER token

--- lexer.rkt
+++ lexer.rkt
@@ -3,6 +3,11 @@
 
 (define-lex-abbrev digits (:+ (char-set "0123456789")))
 
+(define-lex-abbrev lower-latin (char-set "abcdefghijklmnopqrstuvwxyz"))
+(define-lex-abbrev upper-latin (char-set "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+(define-lex-abbrev nondigits (:+ (:or lower-latin upper-latin (char-set "_"))))
+
 (define sdf-lexer
   (lexer-srcloc
     ["\n" (token 'NEWLINE lexeme)]
@@ -10,7 +15,9 @@
     [(from/stop-before "//" "\n") (token lexeme #:skip? #t)]
     [(:or "module" "endmodule" "input" "output"
       "(" ")" ";" "," "+" "-" "/" "*") (token lexeme lexeme)]
-    [(:or (:seq digits (:? "." digits)))
-      (token 'CONSTANT (string->number lexeme))]))
+    [(:seq digits (:? "." digits))
+      (token 'CONSTANT (string->number lexeme))]
+    [(:seq nondigits (:or nondigits digits))
+      (token 'IDENTIFIER lexeme)]))
 
 (provide sdf-lexer)
